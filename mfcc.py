cd23480c8fabf5d74608dc799cc7860072aa18a3
alicolak35 <101671940+alicolak35@users.noreply.github.com>
2023-02-20 21:02:49 +0300

    Update mfcc.py

--- mfcc.py
+++ mfcc.py
@@ -4,29 +4,12 @@ import numpy as np
 import librosa
 from librosa import feature
 from glob import glob
-import pandas as pd
 import matplotlib.pyplot as plt
 import librosa.display
-import csv
+
 
 dosya_1="audio_and_txt_filess"
 ses_dosyalari=glob(dosya_1 + "/*.wav")
-print(ses_dosyalari[712])
-
-for i in range(len(ses_dosyalari)):
-    if "179_1b1_Tc_sc_Meditron_sr200_time_shifted.wav" in ses_dosyalari[i]:
-        print(i)
-
-filenames=[f for f in listdir(dosya_1) if (isfile(join(dosya_1,f)) and f.endswith(".wav"))]
-
-
-p_id_in_file=[]
-for name in filenames:
-    p_id_in_file.append(int(name[:3]))
-
-p_id_in_file= np.array(p_id_in_file)
-## mfcc features ekleyim
-##makine öğrenmesiyle daha etkili sonuç aalbilirim, mö+dö
 
 
 def get_spectrogram(file):
@@ -41,6 +24,7 @@ def get_spectrogram(file):
     plt.axis("off")
 
     return plt.savefig(f"{file}.png", bbox_inches="tight", pad_inches=0)
+
 
 for file in ses_dosyalari:
 
