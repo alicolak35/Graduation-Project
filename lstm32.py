575b2ace4a43ed7b27253a1fd27a2e22eed519d9
alicolak35 <101671940+alicolak35@users.noreply.github.com>
2023-02-20 21:05:14 +0300

    Add files via upload

---
+++ lstm32.py
@@ -0,0 +1,81 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, LabelEncoder
+from keras.utils import to_categorical
+from keras.models import Sequential
+from keras.layers import LSTM, Dense, SimpleRNN
+import tensorflow as tf
+import numpy as np
+import seaborn as sns
+from imblearn.over_sampling import SMOTE
+
+# Load the dataset
+data = pd.read_csv("merged_augmented_data.csv")
+
+
+# Preprocess the data
+#features = data["Features"].str.split().apply(lambda x: [float(i) for i in x])
+features = data["Features"].str.replace("[","").str.replace("]","").str.split().apply(lambda x: [float(i) for i in x])
+features = features.values.tolist()
+features = StandardScaler().fit_transform(features)
+labels = data["diagnosis"]
+labels = LabelEncoder().fit_transform(labels)
+labels = to_categorical(labels)
+
+# Split the data into train and test sets
+X_train, X_test, y_train, y_test = train_test_split(features, labels, test_size=0.2, random_state=42)
+
+# Design of the model
+model = Sequential()
+model.add(LSTM(128, input_shape=(X_train.shape[1], 1)))
+model.add(Dense(128, activation='relu'))
+#model.add(Dense(256, activation='relu'))
+model.add(Dense(2, activation='softmax'))
+
+# Compile the model
+model.compile(loss='categorical_crossentropy', optimizer='adam', metrics=['accuracy'])
+
+# Train the model
+model.fit(X_train, y_train, batch_size=8, epochs=50, validation_data=(X_test, y_test))
+
+# Evaluate the model
+test_loss, test_acc = model.evaluate(X_test, y_test)
+print('Test accuracy:', test_acc)
+
+from sklearn.metrics import classification_report, confusion_matrix
+
+import matplotlib.pyplot as plt
+from sklearn.metrics import confusion_matrix, classification_report, accuracy_score
+
+# get predictions
+y_pred = model.predict(X_test)
+y_pred = np.argmax(y_pred, axis=1)
+
+# get the confusion matrix
+cm = confusion_matrix(np.argmax(y_test, axis=1), y_pred)
+
+# get classification report
+cr = classification_report(np.argmax(y_test, axis=1), y_pred)
+
+# get accuracy
+acc = accuracy_score(np.argmax(y_test, axis=1), y_pred)
+
+
+
+# print results
+print("Confusion Matrix: \n", cm)
+print("Classification Report: \n", cr)
+print("Accuracy: ", acc)
+
+ax = sns.heatmap(cm, annot=True, fmt='d')
+
+print(model.summary())
+
+#plot_model(model,to_file="rnnlayers.png")
+#plt.show()
+
+
+# Save the model
+#model.save('Rnn8-dense128.h5')
+#tf.lite.TFLiteConverter.from_saved_model("rnn8-aug")  16.01:olmadı bu şekilde
+#tf.keras.models.save_model(model, 'path/to/save/rnn128')
---
+++ merging.py
@@ -0,0 +1,98 @@
+from os import listdir
+from os.path import isfile,join
+import numpy as np
+import librosa
+from librosa import feature
+from glob import glob
+import pandas as pd
+import matplotlib.pyplot as plt
+import librosa.display
+import csv
+
+dosya_1="audio_and_txt_filess"
+ses_dosyalari=glob(dosya_1 + "/*.wav")
+ses_dosyalari.index("148_1b1_Al_sc_Meditron_sr200_time_shifted")
+
+
+filenames=[f for f in listdir(dosya_1) if (isfile(join(dosya_1,f)) and f.endswith(".wav"))]
+"""""
+
+p_id_in_file=[]
+for name in filenames:
+    p_id_in_file.append(int(name[:3]))
+
+p_id_in_file= np.array(p_id_in_file)
+## mfcc features ekleyim
+##makine öğrenmesiyle daha etkili sonuç aalbilirim, mö+dö
+
+fn_list_i = [
+        feature.chroma_stft,
+        feature.spectral_centroid,
+        feature.spectral_bandwidth,
+        feature.spectral_rolloff,
+
+    ]
+
+fn_list_ii = [
+
+        feature.zero_crossing_rate
+
+    ]
+def get_feature_vector(y, sr):
+
+    feat_vect_i = [np.mean(funct(y, sr)) for funct in fn_list_i]
+    feat_vect_ii = [np.mean(funct(y)) for funct in fn_list_ii]
+
+    feature_vector = np.round(feat_vect_i, 2) + np.round(feat_vect_ii, 2)
+    #feature_vector = np.round(feature_vector, 2)
+    return feature_vector
+
+filepaths=[join(dosya_1,f) for f in filenames]
+
+tani_dosyasi=pd.read_csv("patient_diagnosis1.csv",header=None)
+labels = np.array([tani_dosyasi[tani_dosyasi[0] == x][1].values[0] for x in p_id_in_file])
+# build the matrix with normal audios featurized
+audios_feat = []
+for file in ses_dosyalari:
+    '''
+    y is the time series array of the audio file, a 1D np.ndarray
+    sr is the sampling rate, a number
+    '''
+    y, sr = librosa.load(file, sr=4000)
+    feature_vector = get_feature_vector(y, sr)
+    #feature_vector=np.round(feature_vector,2)
+    audios_feat.append(feature_vector)
+
+print("finished feature extr. from: ", len(audios_feat))
+
+audios_feat=np.array(audios_feat)
+flat_features = [f.flatten() for f in audios_feat]
+
+# Create a dataframe with the audio features and the ID
+df = pd.DataFrame({'ID': p_id_in_file, 'Features': flat_features})
+
+# Write the dataframe to a CSV file
+df.to_csv("audio_and_text.csv", index=False)
+
+# Read the audio features file
+audio_features = pd.read_csv("audio_and_text.csv")
+"""
+"""""
+# Read the diagnosis file
+diagnosis = pd.read_csv("patient_diagnosis1.csv",header=None)
+
+# Rename the column of diagnosis
+diagnosis = diagnosis.rename(columns={0: "ID", 1: "diagnosis"})
+
+# Merge the two dataframes on the ID column
+merged_data = pd.merge(audio_features, diagnosis, on='ID')
+
+# Write the merged data to a new CSV file
+merged_data.to_csv("augmented_data.csv", index=False)
+
+
+
+
+
+
+
